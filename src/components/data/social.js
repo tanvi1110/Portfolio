aee2728a33e3e26b7664e028bc36686b47d4e8aa
Tanvi <tanvi221110@gmail.com>
2022-01-28 13:10:04 -0800

    social(updated)

---
+++ src/components/data/social.js
@@ -0,0 +1,27 @@
+export const SocialData = [
+    {
+        platform: "Intagram",
+        link:"https://www.instagram.com/tanvi_1304/",
+        icon: require('../../assets/icons/instagram.png')
+    },
+    {
+        platform: "Twitter",
+        link:"https://mobile.twitter.com/TanviSh70149225",
+        icon: require('../../assets/icons/twitter.png'),
+    },
+    {
+        platform: "Facebook",
+        link:"https://kj   instagram.com/tanvi_1304/ ",
+        icon: require('../../assets/icons/facebook.png'),
+    },
+    {
+        platform: "Linkedin",
+        link:"https://www.linkedin.com/mwlite/in/tanvi-shah-4b2159225",
+        icon: require('../../assets/icons/linkedin.png'),
+    },
+    {
+        platform: "Github",
+        link:"https://github.com/tanvi1110",
+        icon: require('../../assets/icons/github.png'),
+    },
+];
