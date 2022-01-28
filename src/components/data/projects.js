99fa6992438811617b0929d99e8d3d509986d95b
Tanvi <tanvi221110@gmail.com>
2022-01-28 14:08:37 -0800

    updated

---
+++ src/components/data/projects.js
@@ -0,0 +1,21 @@
+export const ProjectData=[
+    {
+        id:1,
+        title: "Space-Hub",
+        about: "A website to wonders to universe ",
+        tags: ["Html", "CSS"],
+        github: 'https://github.com/tanvi1110/Space-Hub',
+        image: "https://miro.medium.com/max/1400/0*yJ6UCbVLoTOIbpDN.png",
+ 
+
+    },
+    {
+        id:2,
+        title: "Portfolio",
+        about: " A sample Portfolio 1.0 ",
+        tags: ["Html", "CSS", "ReactJs", "JS", "JSX","NodeJs" ],
+        github: 'https://github.com/tanvi1110/Portfolio',
+        image: "https://static.wixstatic.com/media/0e0314_b069ac6107c9410b9b5087c0862419de~mv2.jpg/v1/fill/w_924,h_583,al_c,q_90/0e0314_b069ac6107c9410b9b5087c0862419de~mv2.jpg",
+ 
+    }
+]
