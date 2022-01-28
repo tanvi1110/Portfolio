35a37ce560ddea218b141986a0197b77809b0206
Tanvi <tanvi221110@gmail.com>
2022-01-28 14:05:38 -0800

    project(updated)

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { ProjectData } from '../../data/projects';
+import ProjectCard from "./project-card";
 import './projects.css';
 function Projects() {
-  return <div>This is projects</div>;
+    const data= ProjectData;
+  return <div className='projects'>
+      <label className='section-title'>Projects</label>
+      <div>
+          {data.map((project)=>{
+            return <ProjectCard project={project} />
+          })}
+      </div>
+
+  </div>;
 }
 
 export default Projects;
