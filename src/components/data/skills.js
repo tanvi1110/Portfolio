d906ee718bd432fa7984305f4dd2f91b2ddff5f7
Tanvi <tanvi221110@gmail.com>
2022-02-01 06:15:06 -0800

    export skilldata

---
+++ src/components/data/skills.js
@@ -0,0 +1,55 @@
+export const SkillsData=[
+    {
+        type:"Frontend",
+        list:[
+            {
+                name:"ReactJS",
+                icon:<i class="devicon-react-original colored"></i>
+            }
+            ,
+            {
+                name:"Javascript",
+                icon:<i class="devicon-javascript-plain colored"></i>
+            }
+            ,
+            {
+                name:"Html",
+                icon:<i class="devicon-html5-plain colored"></i>
+            }
+            ,
+            {
+                name:"CSS",
+                icon:<i class="devicon-css3-plain colored"></i>
+            }
+        ]
+    },
+    {
+        type: "Backend",
+        list: [
+            { 
+                name:"C++",
+                icon: <i class="devicon-cplusplus-plain colored"></i>
+            }
+            ,
+            {
+                name:"Java",
+                icon: <i class="devicon-java-plain colored"></i>
+
+
+            }
+            ,
+            {
+                name:"C",
+                icon: <i class="devicon-c-plain colored"></i>
+
+            }
+             ,
+             {
+                name:"Python",
+                icon:  <i class="devicon-python-plain colored"></i>
+
+            }          
+        ]
+    }
+
+]
