0aac4ef0da0973b701b93b9483806036b0658bc2
Tanvi <tanvi221110@gmail.com>
2022-02-01 10:48:01 -0800

    modified_1

--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import Seperator from '../../common/seperator';
-// import './work.css';
-// import {WorkData} from '../../data/work';
-// function Work() {
-//   const data = WorkData;
-//   return (
-//   <div className='work'>
-//      <Seperator /> 
-//     <label className='section-title'></label>
-//    <div className='work-list'>
-//      {data.map((item)=>{
-//        return
-//      })}
-//    </div>
-//   </div>
-//   );
-// }
+import React from 'react';
+import Seperator from '../../common/seperator';
+import './work.css';
+import {WorkData} from '../../data/work';
+function Work() {
+  const data = WorkData;
+  return (
+  <div className='work'>
+     {/* <Seperator />  */}
+    <label className='section-title'></label>
+   <div className='work-list'>
+     {data.map((item)=>{
+       return
+     })}
+   </div>
+  </div>
+  );
+}
 
-// export default Work;
+export default Work;
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -6,18 +6,18 @@ export const ProjectData=[
         tags: ["Html", "CSS"],
         demo: "link.com ",
         github: 'https://github.com/tanvi1110/Space-Hub',
-        image: "https://miro.medium.com/max/1400/0*yJ6UCbVLoTOIbpDN.png",
+        image: require("../../assets/project/project2.jpg"),
  
 
     },
     {
         id:2,
-        title: "Portfolio",
+        title: "Portfolio 1.0",
         about: " A sample Portfolio 1.0. a sample portfolio to look and have ideas gra  sph . Still in progress, underwork.",
         tags: ["Html", "CSS", "ReactJs", "JS", "JSX","NodeJs" ],
         demo: "link2.com ",
         github: 'https://github.com/tanvi1110/Portfolio',
-        image: "https://static.wixstatic.com/media/0e0314_b069ac6107c9410b9b5087c0862419de~mv2.jpg/v1/fill/w_924,h_583,al_c,q_90/0e0314_b069ac6107c9410b9b5087c0862419de~mv2.jpg",
+        image: require("../../assets/project/project1.jpg"),
  
     }
 ]   
