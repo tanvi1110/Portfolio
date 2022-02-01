2d3f6607959df0f012eb36de28b2d4f19ca491ef
Tanvi <tanvi221110@gmail.com>
2022-02-01 06:08:49 -0800

    seperator

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Seperator from '../../common/seperator';
+import SocialContact from '../../common/social-contact';
 import './contact.css';
 function Contact() {
-  return <div>This is contact</div>;
+  return (
+  <div className='contact'>
+    <Seperator />
+    <label className='section-title'>Contact</label>
+    <div className='contact-container'>
+      <div className='contact-left'>
+        <p>Get in touch.Contact Info</p>
+       <SocialContact />
+      </div>
+      <div className='download'>
+         {/* <a download href={require('../../../assets/resume.pdf').default}>
+           
+           Download resume 
+            </a> */}
+    </div>
+
+   
+   
+    </div>
+    
+    
+    </div>
+  );
 }
 
 export default Contact;
