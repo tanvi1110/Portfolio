e43d8a815f4290b160b4d7cd2566539c17a84736
Tanvi <tanvi221110@gmail.com>
2022-02-01 06:16:10 -0800

    workdata export

---
+++ src/components/data/work.js
@@ -0,0 +1,20 @@
+export const WorkData=[
+    {
+        company: "1",
+        designation:"default 1 ",
+        dateJoining: "22/13/2021",
+        dateEnd:"Present",
+        companyLogo:"example.png",
+        work: " dskjgfjhdfjdkljlsdjfljd",
+
+    },
+    {
+        company: "2",
+        designation:"default 2 ",
+        dateJoining: "22/13/2021",
+        dateEnd:"Present",
+        companyLogo:"example2.png",
+        work: " dskjgfjhdfjdkljlsdjdsfdfljd",
+
+    }
+]
