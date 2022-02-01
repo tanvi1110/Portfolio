745b12fada747793370c10afc04794dd06e50114
Tanvi <tanvi221110@gmail.com>
2022-02-01 06:10:05 -0800

    data.map

--- src/components/body/skills/index.js
+++ src/components/body/skills/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import Seperator from '../../common/seperator';
 import './skills.css';
+import {SkillsData}  from "../../data/skills";
+import SkillCard from './skill-card';
 function Skills() {
-  return <div>
-      This is Skills
+  const data = SkillsData;
+  return <div className='skills'>
+       <Seperator/> 
+      <label className='section-title'>Skills</label>
+     <div>
+       {data.map((item)=>{
+         return(
+           <div className='skills-section'>
+                <label className='skills-section-title'>{item.type}</label>
+                 <div className='skills-list'> 
+                  {item.list.map((skill)=>{
+                    return(
+                      <SkillCard skill={skill} />
+                    )           
+                  })}
+                  </div>
+           </div>
+         )
+       })}
+     </div>
   </div>;
 }
 
