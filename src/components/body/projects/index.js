2b3577217032b771bef292ec278796fad923bfd3
Tanvi <tanvi221110@gmail.com>
2022-02-01 06:09:14 -0800

    seperator-

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Seperator from '../../common/seperator';
 import { ProjectData } from '../../data/projects';
 import ProjectCard from "./project-card";
 import './projects.css';
 function Projects() {
     const data= ProjectData;
   return <div className='projects'>
+      <Seperator />
       <label className='section-title'>Projects</label>
       <div>
           {data.map((project)=>{
